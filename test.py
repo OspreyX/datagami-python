6a9062d1b505ccd19622cac80000a0186a0b4204
Keiran Thompson <thompson.keiran@gmail.com>
2014-07-06 00:52:22 +0800

    Update test script

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 
 import numpy as np
 import datagami
+import pprint
 
 x = np.arange(100)
 y = np.sin(x*np.pi/10)*x*0.1 + np.random.normal(size=100)
 
 # f = datagami.TimeSeries1D(y).forecast('RQ', 5)
 
-dg = datagami.TimeSeries1D(y)
-res = dg.auto(kernel_list=["SE","RQ","SE + RQ","Lin*SE"], out_of_sample_size=10)
+dg = datagami.forecast1D(y, 'RQ', 10)
+
+pprint.pprint(dg, indent=4)
+
+res = datagami.auto1D(y, kl=["SE","RQ","SE + RQ","Lin*SE"], n=10)
+
+pprint.pprint(res, indent=4)
 
 
 
