0159fc5793dde64419328e25d22119eb8c86fa29
Alex Louden <alex@louden.com>
2015-01-08 15:59:41 +0800

    Setup travis with secure api secret

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-def run_tests():
-    import nose
-    nose.main()
-
-if __name__ == '__main__':
-    run_tests()
--- tests/test_timeseries_1D.py
+++ tests/test_timeseries_1D.py
@@ -1,24 +1,14 @@
+import os
 import datagami
 
-API_URL = ''
-KEY = ''
-SECRET = ''
-
-
-def test_linear():
-    data = range(100)
-    ts = datagami.TimeSeries1D(data, KEY, SECRET, url=API_URL)
-
-    result = ts.forecast('SE', 10)
-
-    rounded = [int(p + 0.5) for p in result['predicted']]
-    assert rounded == range(100, 110)
+KEY = os.environ.get('TEST_API_KEY')
+SECRET = os.environ.get('TEST_API_SECRET')
 
 
 def test_linear_convenience():
 
     data = range(100)
-    result = datagami.forecast1D(data, KEY, SECRET, k='SE', n=10, url=API_URL)
+    forecast = datagami.forecast1D(data, KEY, SECRET)
 
-    rounded = [int(p + 0.5) for p in result['predicted']]
+    rounded = [int(p + 0.5) for p in forecast['predicted']]
     assert rounded == range(100, 110)
